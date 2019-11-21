5aa9f300e7cbc86adbf3fcccd4de6388d106cda9
Marie-Cathrine Sørensen <mari342e@edu.eal.dk>
2019-11-21 10:11:28 +0100

    Creating new component for navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navigation"></div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
