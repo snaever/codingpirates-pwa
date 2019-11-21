e9d3eda7b24f95f4241156523358aa5b2ea5d33a
Kasper Snæver <kasper@snaever.dk>
2019-11-21 14:29:24 +0100

    Creating chat

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="chat">
         <Topbar pageTitle="Besked" />
-            Hej
+            <div class="container">
+                <div class="messages">
+
+                    <div class="message sender">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse erat eros, eleifend non quam vel, efficitur molestie diam. Suspendisse eget mauris nec enim pulvinar aliquam et eu eros. Proin dignissim arcu quis venenatis malesuada. Duis vestibulum, risus ut placerat malesuada, turpis turpis feugiat dui, et consequat ex sapien a lectus.</p>
+                    </div>
+
+                    <div class="message receiver">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse erat eros, eleifend non quam vel, efficitur molestie diam. Suspendisse eget mauris nec enim pulvinar aliquam et eu eros. Proin dignissim arcu quis venenatis malesuada. Duis vestibulum, risus ut placerat malesuada, turpis turpis feugiat dui, et consequat ex sapien a lectus.</p>
+                    </div>
+
+                </div>
+            </div>
         <Navigation />
     </div>
 </template>
@@ -19,3 +31,32 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_all.scss';
+
+.messages {
+    margin-top: $barsHeight + 20px;
+    position: relative;
+    height: calc(100vh - #{$barsHeight} - #{$barsHeight});
+}
+
+.message {
+    padding: 15px;
+    margin-top: 20px;
+
+    p {
+        margin: 0;
+    }
+
+    &.sender {
+        background-color: $dark-gray;
+        margin-left: 15%;
+    }
+
+    &.receiver {
+        background-color: $light-gray;
+        margin-right: 15%;
+    }
+}
+</style>
