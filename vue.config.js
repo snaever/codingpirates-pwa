e524422b993203514a0923f187f761cf7f481c79
Kasper Snæver <kasper@snaever.dk>
2019-12-03 11:42:30 +0100

    Configuring PWA

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    pwa: {
+      name: 'Coding Pirates',
+      themeColor: '#00AEEF',
+      msTileColor: '#12120e',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'default',
+      iconPaths: {
+        favicon32: 'icons/favicon-32x32.png',
+        favicon16: 'icons/favicon-16x16.png',
+        appleTouchIcon: 'icons/apple-touch-icon-152x152.png',
+        maskIcon: 'icons/safari-pinned-tab.svg',
+        msTileImage: 'icons/mstile-144x144.png'
+      }
+    }
+  }
