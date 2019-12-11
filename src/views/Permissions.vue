73a4019f68c6543ae600df403ac533838c5c378b
Marie-Cathrine Sørensen <mari342e@edu.eal.dk>
2019-12-11 14:36:27 +0100

    Adding style to permission page

--- src/views/Permissions.vue
+++ src/views/Permissions.vue
@@ -2,6 +2,20 @@
   <div class="tilladelser">
     <Topbar pageTitle="Tilladelser" />
     <div class="container">
+      <div class="item">
+        <div class="item-title">Der må tages billeder af mit barn</div>
+        <label class="switch">
+          <input type="checkbox">
+          <span class="slider round"></span>
+        </label>
+      </div>
+      <div class="item">
+        <div class="item-title">Billeder af mit barn på bruges offentligt</div>
+        <label class="switch">
+          <input type="checkbox">
+          <span class="slider round"></span>
+        </label>
+      </div>
     </div>
     <Navigation />
   </div>
@@ -30,5 +44,87 @@ export default {
   display: flex;
   flex-direction: column;
   overflow: scroll;
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+
+  /* Hide default HTML checkbox */
+  input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  /* The slider */
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: $dark-gray;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+
+  input:checked + .slider {
+    background-color: $blue;
+  }
+
+  input:focus + .slider {
+    box-shadow: 0px;
+  }
+
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+
+  .slider.round:before {
+    border-radius: 50%;
+  }
+}
+
+.item {
+  border-bottom: 1px solid $dark-gray;
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .more-icon {
+    width: 35px;
+    height: 35px;
+  }
+
+  .item-title {
+    padding-left: 10px;
+    font-size: 13pt;
+    font-weight: bold;
+  }
 }
 </style>
