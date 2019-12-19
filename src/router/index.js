f47b5eb1ece1fd5aaf8e12940cc60c7e9feb2f68
Kasper Snæver <kasper@snaever.dk>
2019-12-19 08:29:01 +0100

    Changing route name

--- src/router/index.js
+++ src/router/index.js
@@ -134,7 +134,7 @@ const routes = [
     }
   },{
     path: '/beskeder/ny',
-    name: 'besked',
+    name: 'ny-besked',
     component: () => import('../views/messages/MessageCreate.vue'),
     meta: {
       title: 'Ny besked'
